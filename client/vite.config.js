3446777e707aed87347948ad53388f2cdd919808
Shayan Khanani <shani.khanani@gmail.com>
2025-10-01 19:47:51 +0500

    new commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "https://my-todo-app-phi-olive.vercel.app/", 
+        target: "https://todo-backend-two-chi.vercel.app/api", 
         changeOrigin: true,
         secure: true, 
       },
